e527f6bcafd326b4e4751491123ac67cce0064b3
Lazarus22 <matthew.peters.nyc@gmail.com>
2020-09-02 11:45:32 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@
 // need to add a patch request on each characters form to edit their calories on both the page and the database
 
 
+// last 10 minute observations
+// I have all there characters loading when I only want 1 character to laod, I need to use quesry selct to define the html elements and replace them rather then just appending everthing to the bottom
+// I messed up my base url and dom content spelling for most of the exam it really messed me up -- such a dumb mistake
 
 
 
 
 
-document,addEventListener('DOMCOontentLoaded', () => {
+
+document,addEventListener('DOMContentLoaded', () => {
     const BASEURL = 'http://localhost:3000/characters/'
 
     //fetch characters from server
     const getCharacters = () => {
         fetch(BASEURL)
-        .then(resp => resp.jason())
+        .then(resp => resp.json())
         .then(renderCharacters)
     }
 
@@ -27,34 +31,39 @@ document,addEventListener('DOMCOontentLoaded', () => {
         }
     }
 
-    //render names in the top bar
-    const renderCharacter = (characterObj) => {
-    const characterDiv = document.querySelector("#character-bar")
-    const characterNamesSpan = document.createElement('div')
-    //you say to use span but the names also have to be a button shows character detail
-    characterNamesSpan.innerHTML = `
-    <span>${characterObj.name}</span> 
-    `
-    }
+    // render names in the top bar
+    // const renderCharacter = (characterObj) => {
+    // const characterDiv = document.querySelector("#character-bar")
+    // const characterNamesSpan = document.createElement('div')
+    // //you say to use span but the names also have to be a button shows character detail
+    // characterNamesSpan.innerHTML = `
+    // <span data-id='${characterObj.id}'>${characterObj.name}</span> 
+    // `
+    // }
 
-renderCharacters()
+    
 
 
 
     //detailed info render instructions
-    // const renderCharacter = (characterObj) => {
-    //     const characterDiv = document.querySelector('#detailed-info')
-    //     const characterSpan = document.createElement('div')
-    //     characterSpan.className = 'characterInfo'
-    //     characterSpan.innerHTML =`
-    //         <p id="name">${characterObj.name}</p>
-    //         <img id="image" src="${characterObj.image}"><!-- display character image here -->
-    //         <h4>Total Calories: <span id="calories">${characterObj.calories}</span> </h4>
-    //     `
-    //     characterDiv.append(characterSpan)
-    // }
+    const renderCharacter = (characterObj) => {
+        const characterDiv = document.querySelector('#detailed-info')
+        const characterSpan = document.createElement('div')
+        const name = document.querySelector('name')
+        const image = document.querySelector('image')
+        const calories = document.querySelector('calories')
+        characterSpan.className = 'characterInfo'
+        characterSpan.innerHTML =`
+            <p id="name">${characterObj.name}</p>
+            <img id="image" src="${characterObj.image}"><!-- display character image here -->
+            <h4>Total Calories: <span id="calories">${characterObj.calories}</span> </h4>
+        `
+        characterDiv.append(characterSpan)
+    }
     // getCharacters()
-
+            // name = ${characterObj.name}
+            // image = ${characterObj.image
+            // calories = ${characterObj.calories}
 
     //form that adds calories
     function form (){
@@ -78,6 +87,21 @@ renderCharacters()
 
     })}
 
-   form()
+   
 
+    //event listener for character name bar itmes
+    function clicker() {
+        document.addEventListener("cliick", (e) => {
+            console.log("you are int he clicker")
+
+        }
+        
+        )
+    }
+
+
+
+form()
+getCharacters()
+clicker()
 })//dom content loaded
