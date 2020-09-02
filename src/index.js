9406f55e4c1a66723ba5bdaa3c3d865a2052461e
github username <github email address>
2020-09-02 11:45:40 -0400

    Incomplete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     let charDiv = document.querySelector('#character-bar')
+    // console.dir(charDiv)
+    let charDetail = document.querySelector('#detailed-info')
+    // console.log(charDetail)
     
     function fetchChars() {
         fetch('http://localhost:3000/characters/')
@@ -8,5 +11,24 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     fetchChars();
 
+    function renderChar(char) {
+        let charSpan = document.createElement('span')
+        // charSpan.dataset.id = charId
+        charSpan.innerHTML = char.name
+        charDiv.append(charSpan)
+    }
 
+    function renderChars(charsData) {
+        charsData.forEach(charData => {
+            renderChar(charData)
+        });
+    }
+
+    charDiv.addEventListener('click', function(e){
+        if(e.target.nodeName === 'SPAN'){
+            const charName = e.target
+            // const allNames = charName.innerHTML
+            console.log(charName)
+        }
+    })
 });
