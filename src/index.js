2e39e3b583c1a195a84d6857c6dbc9debd678d6a
Mimioj101 <mimioj@gmail.com>
2020-09-02 10:44:32 -0400

    last deliverable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     const characterDiv = document.querySelector('#character-bar')
     const charCal = document.querySelector('#calories')
     const detailedInfo = document.querySelector('#detailed-info')
+    const calForm = document.querySelector('#calories-form')
 
     function fetchCharacters() {
         fetch(baseUrl)
@@ -20,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
         characterSpan.className = 'character-span'
         characterSpan.innerText = `${character.name}`
         characterDiv.append(characterSpan)
-    
+    }
 
     // add click listener to CharacterSpan
     // on click, grab the delatailed-info div
@@ -28,44 +29,60 @@ document.addEventListener("DOMContentLoaded", function(e) {
     // set detailed-info div.name = character.name & image = character.image
     // will need to set calories = charaters' calories but its in a separate h4
 
-}
-            const clickHandler = () => {
-                document.addEventListener('click', function(e){
-                    if (e.target.matches('.character-span')) {
-                        const button = e.target
-                        const id = button.dataset.id
-    
-                        fetch(baseUrl + id)
-                        .then(resp => resp.json())
-                        .then(character => renderOneCharacter(character))
 
-                        const renderOneCharacter = (character) => {
-                            console.log(character)
-                            // console.log(detailedInfo.childNodes[3]['src'])
-                            detailedInfo.childNodes[1]['textContent'] = character.name
-                            detailedInfo.childNodes[3]['src'] = character.image
-                            console.log(charCal.innerText)
+    const clickHandler = () => {
+        document.addEventListener('click', function(e){
+            if (e.target.matches('.character-span')) {
+                const button = e.target
+                const id = button.dataset.id
 
-                        }
+                fetch(baseUrl + id)
+                .then(resp => resp.json())
+                .then(character => renderOneCharacter(character))
 
-
-                        
-
-                    }
-    
-    
-                })
+                const renderOneCharacter = (character) => {
+                    // console.log(character)
+                    // console.log(detailedInfo.childNodes[3]['src'])
+                    detailedInfo.childNodes[1]['textContent'] = character.name
+                    detailedInfo.childNodes[3]['src'] = character.image
+                    charCal.innerText = character.calories
+                }
             }
+        })
+    }
+
+    // add submit listener to whole form + e.preventDefault
+    // find the current cal in the DOM
+    // parseIn current cal
+    // find input.value in form
+    // add that number to the current cal
+    // set the cal node's inner text to new current cal
+    // fetch request (PATCH + options) (will need id)
+    const submitHandler = () => {
+        calForm.addEventListener('submit', function(e){
+            e.preventDefault()
+            const button = e.target
+            const cal = charCal.innerText
+            const currentCal = parseInt(cal, 10)
+            // console.log(typeof currentCal)
+            // console.dir(typeof button[1]["value"])
+            const upCal = button[1]["value"]
+            const addCals = parseInt(upCal, 10)
+            const updatedCals = currentCal + addCals
+            // console.log(updatedCals)
+            cal.innerText = updatedCals
+
+
+        })
+    }
     
     
-    
-            clickHandler()
 
 
 
-    
 
+    submitHandler()
+    clickHandler()
     fetchCharacters()
-
 
 })
