098af9171cd2320aedfd4dadcaba77d47a71e708
Lazarus22 <matthew.peters.nyc@gmail.com>
2020-09-02 10:44:38 -0400

    message

--- src/index.js
+++ src/index.js
@@ -1,30 +1,49 @@
 document,addEventListener('DOMCOontentLoaded', () => {
     const BASEURL = "http://localhost:3000/characrters/"
 
+    //fetch characters from server
     const getCharacters = () => {
         fetch(BASEURL)
         .then(resp => resp.jason())
         .then(renderCharacters)
     }
 
+    //render iteration
     const renderCharacters = (characters) => {
         for (const character of characters){
-            renderCharacter(character)
+            renderNames(character)
         }
     }
 
-    const renderCharacter = (characterObj) => {
-        const characterDiv = document.querySelector('#detailed-info')
-        const characterSpan = document.createElement('div')
-        characterSpan.className = 'characterInfo'
-        characterSpan.innerHTML =`
-            <p id="name">${characterObj.name}</p>
-            <img id="image" src="${characterObj.image}"><!-- display character image here -->
-            <h4>Total Calories: <span id="calories">${characterObj.calories}</span> </h4>
-        `
-        characterDiv.append(characterSpan)
+    //render names in the top bar
+    const renderNames = (nameObj) => {
+    const characterDiv = document.querySelector("#character-bar")
+    const characterNames = document.createElement('div')
+    //you say to use span but the names also have to be a button that runs a post request
+    characterNames.innerHTML = `
+    <span>${nameObj.name}</span> 
+    `
     }
-    getCharacters()
+renderNames()
+
+
+
+    //single render instructions
+    // const renderCharacter = (characterObj) => {
+    //     const characterDiv = document.querySelector('#detailed-info')
+    //     const characterSpan = document.createElement('div')
+    //     characterSpan.className = 'characterInfo'
+    //     characterSpan.innerHTML =`
+    //         <p id="name">${characterObj.name}</p>
+    //         <img id="image" src="${characterObj.image}"><!-- display character image here -->
+    //         <h4>Total Calories: <span id="calories">${characterObj.calories}</span> </h4>
+    //     `
+    //     characterDiv.append(characterSpan)
+    // }
+    // getCharacters()
+
+
+   
 
 
 
