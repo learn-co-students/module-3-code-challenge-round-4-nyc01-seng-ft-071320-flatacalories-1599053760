713f7d350a3cf71f89be6f454957c0823ca07c20
irosen419 <irosen419@gmail.com>
2020-09-02 11:12:00 -0400

    completed all deliverables

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const renderCharacterSpans = (characters) => {
         const characterSpan = ce('span')
         characterSpan.dataset.id = char.id
         characterSpan.innerHTML = `${char.name}`
-        characterBar.append(characterSpan)
+        characterBar.prepend(characterSpan)
     }
 }
 
@@ -28,6 +28,14 @@ const clickHandler = () => {
             let span = e.target
             let charId = span.dataset.id
             fetchChar(charId)
+        } else if (e.target.matches('button')) {
+            characterInfo.innerHTML = `
+            <form id="new-character-form">
+                <input type="text" placeholder="Enter Character Name" name="name" id="name" />
+                <input type="text" placeholder="Image URL Goes Here" name="image" id="image" />
+                <input type="submit" value="Create Character" />
+            </form>
+            `
         }
     })
     characterInfo.addEventListener('click', e => {
@@ -51,8 +59,9 @@ const renderCharInfo = (character) => {
     <h4>Total Calories: <span id="calories">${character.calories}</span> </h4>
     <form id="calories-form">
         <input type="hidden" value="${character.id}" name="id" id="characterId" />
-        <input type="text" placeholder="Enter Calories" name="calories" id="calories" />
-        <input type="submit" value="Add Calories" />
+        <input type="text" value="${character.name}" name="name" id="characterId" />
+        <input type="text" value="0" name="calories" id="calories" />
+        <input type="submit" value="Update Character" />
     </form>
     <button id="reset-btn">Reset Calories</button>
     `
@@ -61,9 +70,13 @@ const renderCharInfo = (character) => {
 const submitHandler = () => {
     document.addEventListener('submit', e => {
         e.preventDefault()
-        const characterForm = qs('#calories-form')
-
-        patchCalories(characterForm)
+        if (e.target.matches('#calories-form')) {
+            const characterForm = e.target
+            patchCalories(characterForm)
+        } else if (e.target.matches('#new-character-form')) {
+            const newForm = e.target
+            postCharacter(newForm)
+        }
     })
 }
 
@@ -74,17 +87,46 @@ const patchCalories = (form, reset = false) => {
     let totalCals = currentCals + newCals
     if (reset) { totalCals = 0 }
 
+    const newObj = {
+        "name": form.name.value,
+        "calories": totalCals
+    }
+
     const options = {
         method: 'PATCH',
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ "calories": totalCals })
+        body: JSON.stringify(newObj)
     }
 
     fetch(baseUrl + id, options)
         .then(resp => resp.json())
         .then(renderCharInfo)
+}
+
+const postCharacter = (form) => {
+    const newChar = {
+        "name": form.name.value,
+        "image": form.image.value,
+        "calories": 0
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newChar)
+    }
+
+    fetch(baseUrl, options)
+        .then(resp => resp.json())
+        .then(renderCharInfo)
+        .then(data => {
+            characterBar.innerHTML = ""
+            fetchCharacters()
+        })
 }
 
 submitHandler()
