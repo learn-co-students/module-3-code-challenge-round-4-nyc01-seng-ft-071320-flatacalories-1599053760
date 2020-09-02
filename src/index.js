d8d789b1a964744ff6bd368762b44ec6c4808cc0
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-09-02 10:43:27 -0400

    submit works

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const baseUrl = "http://localhost:3000/characters/"
+    const charForm = document.querySelector('#calories-form')
+    const charCalories = document.querySelector('#calories')
 
     const getCharacters = () => {
         fetch(baseUrl)
@@ -34,6 +36,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
         charP.innerText = char.name
         charImg.src = char.image
+        charCalories.innerText = char.calories
+
+        charForm.lastElementChild.dataset.id = char.id
     }
 
     function clickHandler() {
@@ -41,7 +46,6 @@ document.addEventListener("DOMContentLoaded", function() {
             let click = e.target
 
             if (click.matches('span')) {
-                console.log(click)
                 let id = click.dataset.id
 
                 fetch(baseUrl + id)
@@ -51,9 +55,46 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    function submitHandler() {
+        charForm.addEventListener("submit", (e) => {
+            e.preventDefault()
+
+            let click = e.target
+            if (click.lastElementChild.value === "Add Calories") {
+                let id = click.lastElementChild.dataset.id
+                let formInput = charForm.calories.value 
+                let oldNum = parseInt(charCalories.innerText)
+
+                if (oldNum === 0) {
+                    charCalories.innerText = formInput
+                } else {
+                    charCalories.innerText = parseInt(oldNum) + parseInt(formInput)
+                }
+                
+                fetch(baseUrl + id, { 
+                    method: "PATCH",
+                    headers: { 
+                        "Content-type": "application/json",
+                        "accept": "application/json"
+                    },
+                    body: JSON.stringify({
+                        calories: parseInt(oldNum) + parseInt(formInput)
+                    })
+                })
+                .then(resp => resp.json())
+                .then(data => {
+
+                })                
+            }
+
+            charForm.reset()
+        })
+    }
+
     
     getCharacters()
     clickHandler()
+    submitHandler()
 })
 
 
@@ -61,5 +102,7 @@ document.addEventListener("DOMContentLoaded", function() {
 1) render all characters name in a div with id of 'character-bar' via GET request
 2) add span tag with characters name to the character bar
 3) when selected characted from character bar, see info inside #detailed-info div
+    - send another GET request with ID to get specific info
+    - update form with character ID
 4) make add calorie button to add caolries, patch request
 */
